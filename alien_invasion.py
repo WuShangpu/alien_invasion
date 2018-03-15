1f349453080c2192fe5d111c726e9ce8d10e177c
wushangpu <wshangpu@gmail.com>
2018-03-15 14:34:58 +0800

    add bg_color

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ def run_game():
 	pygame.init()
 	screen = pygame.display.set_mode((1200, 800))
 	pygame.display.set_caption('biubiubiu~~~')
+	#设置背景颜色
+	bg_color = (230, 230, 230)
 	
 	#开始游戏的主循环
 	while True:
@@ -15,6 +17,9 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+				
+		#每次循环时都重绘屏幕
+		screen.fill(bg_color)
 				
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
